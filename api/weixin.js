a49b80e9b5d4527c3614dc7e6b6e64a2a03c786e
tom <tom.zhu1119@gmail.com>
2016-04-14 21:23:01 +0800

    wx post

--- api/weixin.js
+++ api/weixin.js
@@ -1,9 +1,9 @@
 var sha1 = require('sha1'),
-    events = require('events'),
-    emitter = new events.EventEmitter(),
-    xml2js = require('xml2js');
+  events = require('events'),
+  emitter = new events.EventEmitter(),
+  xml2js = require('xml2js');
 
-var WeiXin = function(){
+var WeiXin = function() {
   this.data = '';
   this.msgType = 'text';
   this.fromUserName = '';
@@ -11,17 +11,74 @@ var WeiXin = function(){
   this.funcFlag = 0;
 }
 
-WeiXin.prototype.checkSignature = function(req){
+WeiXin.prototype.checkSignature = function(req) {
   this.signature = req.query.signature;
   this.timestamp = req.query.timestamp;
   this.nonce = req.query.nonce;
   this.echostr = req.query.echostr;
 
-  var array = [this.token,this.timestamp,this.nonce];
+  var array = [this.token, this.timestamp, this.nonce];
   array.sort();
 
   var str = sha1(array.join(""));
-  return (str == this.signature) 
+  return (str == this.signature)
 }
+
+
+WeiXin.prototype.textMsg = function(callback) {
+  emitter.on('weixinTextMsg', callback);
+  return this;
+}
+
+WeiXin.prototype.parseTextMsg = function() {
+  var msg = {
+    "toUserName": this.data.ToUserName[0],
+    "fromUserName": this.data.FromUserName[0],
+    "createTime": this.data.CreateTime[0],
+    "msgType": this.data.MsgType[0],
+    "content": this.data.Content[0],
+    "msgId": this.data.MsgId[0],
+  };
+
+  emitter.emit('weixinTextMsg', msg);
+  return this;
+}
+
+
+
+WeiXin.prototype.parse = function() {
+  this.msgType = this.data.MsgType[0] ? this.data.MsgType[0] : 'text';
+  switch (this.msgType) {
+    case 'text':
+      this.parseTextMsg();
+      break;
+  }
+}
+
+
+WeiXin.prototype.loop = function(req, res) {
+  this.res = res;
+  var self = this;
+
+  var buf = '';
+  req.setEncoding('utf-8');
+  req.on('data', function(chunk) {
+    buf += chunk;
+  });
+
+  req.on('end', function() {
+    xml2js.parseString(buf, function(err, json) {
+      if (err) {
+        err.status = 400;
+      } else {
+        req.body = json;
+      }
+    });
+
+    self.data = req.body.xml;
+    self.parse();
+  });
+}
+
 
 module.exports = new WeiXin();
--- routes/wxrouter.js
+++ routes/wxrouter.js
@@ -5,13 +5,34 @@ var weixin = require('../api/weixin');
 var config = require('../config/config');
 var aotuConfig = config.wx_config.aotu;
 
-router.get('/',function(req,res,next){
-  if(weixin.checkSignature(req)){
+router.get('/', function(req, res, next) {
+  if (weixin.checkSignature(req)) {
     return res.status(200).send(req.query.echostr);
   }
   return res.render('index');
 })
 
+router.post('/', function(req, res) {
+  weixin.loop(req, res);
+});
+
 weixin.token = aotuConfig.token;
+
+weixin.textMsg(function(msg) {
+  var msgContent = trim(msg.content);
+  var resMsg = {
+    fromUserName: msg.toUserName,
+    toUserName: msg.fromUserName,
+    msgType: 'text',
+    content: 'TOM在不断的成长，欢迎您给出宝贵的意见，有任何疑问请回复 help 或 bz',
+    funcFlag: 0
+  };
+
+
+  weixin.sendMsg(resMsg);
+
+})
+
+
 
 module.exports = router;

b44fa6e4c69a1ef8c3cecee787f7172643302cda
tom_zhu <tom.zhu1119@gmail.com>
2016-04-15 13:42:04 +0800

    wx msg7

--- routes/wxrouter.js
+++ routes/wxrouter.js
@@ -39,35 +39,13 @@ weixin.textMsg(function(msg) {
     var splits = msgContent.split(' ');
     if (splits.length == 2) {
       var city = splits[1];
-      tianqi(city, function(json) {
-        if (json.err) {
-          return weixin.sendMsg(resMsg)
-        }
-        var data = json.msg;
-        if (data.errNum == 0) {
-          var today = data.retData.today;
-          var todayStr = " " + data.retData.city + "天气 " + today.type + "\n";
-          todayStr += "  当前温度 " + today.curTemp + "\n";
-          todayStr += "  最低温度 " + today.lowtemp + "\n";
-          todayStr += "  最高温度 " + today.hightemp + "\n";
-          todayStr += "  风力 " + today.fengli + "\n";
-
-          var todayOtherStr = "\n";
-
-          today.index.forEach(function(item, index) {
-            todayOtherStr += " " + (++index) + "." + item.name + " " + item.index + " " + item.details;
-          });
-          resMsg.content = todayStr + todayOtherStr;
-          return weixin.sendMsg(resMsg);
-        } else {
-          return weixin.sendMsg(resMsg)
-        }
-      });
+      tianqi(city, tqCallback());
+    } else {
+      tianqi('',tqCallback());
     }
   } else {
     flag = true;
   }
-
   // 去掉前后空格并且转换成大写
   function trim(str) {
     return ("" + str).replace(/^\s+/gi, '').replace(/\s+$/gi, '').toUpperCase();
@@ -76,7 +54,37 @@ weixin.textMsg(function(msg) {
   function isKeyInStr(str, key) {
     str = trim(str);
     key = trim(key);
-    return /^(key)*?(\s)*(\w)*/g.test(str);
+    if (str.indexOf(key) !== -1) {
+      return true;
+    }
+    return false;
+  }
+
+  function tqCallback() {
+    return function(json) {
+      if (json.err) {
+        return weixin.sendMsg(resMsg)
+      }
+      var data = json.msg;
+      if (data.errNum == 0) {
+        var today = data.retData.today;
+        var todayStr = " " + data.retData.city + "天气 " + today.type + "\n";
+        todayStr += "  当前温度 " + today.curTemp + "\n";
+        todayStr += "  最低温度 " + today.lowtemp + "\n";
+        todayStr += "  最高温度 " + today.hightemp + "\n";
+        todayStr += "  风力 " + today.fengli + "\n";
+
+        var todayOtherStr = "";
+
+        today.index.forEach(function(item, index) {
+          todayOtherStr += "\n " + (++index) + "." + item.name + " " + item.index + " " + item.details;
+        });
+        resMsg.content = todayStr + todayOtherStr;
+        return weixin.sendMsg(resMsg);
+      } else {
+        return weixin.sendMsg(resMsg)
+      }
+    }
   }
 
   if (flag) {
